5cddb4b527865a5ecfa74a94f8b9b98f683a8514
admin <admin@adminui-noteubug.local>
2024-07-27 04:37:13 +0900

    feat: slotmachine 레이아웃 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Map from "../components/Map";
+import Modal from "../components/Modal";
+import SlotMachine from "../components/SlotMachine";
 
 const Home = () => {
-  return <Map currentIndex={0}></Map>;
+  return (
+    <div className="w-full h-full relative">
+      <Map currentIndex={0}></Map>
+      <Modal>
+        <SlotMachine></SlotMachine>
+      </Modal>
+    </div>
+  );
 };
 
 export default Home;
