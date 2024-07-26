864f0ba2ac6622f8cc031bdc163213afaab1cc05
admin <admin@adminui-noteubug.local>
2024-07-27 02:10:10 +0900

    feat: 보드판 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,46 @@
 import React from "react";
-import Roulette from "../components/Roulette";
-import { useState } from "react";
 
 const Home = () => {
   return (
-    <div className="bg-red-200 w-full">
-      <Roulette />
+    <div className="bg-red-200 w-full h-screen flex justify-center items-center p-3">
+      <div className="w-[95%] h-[90%]">
+        <div className="relative w-full h-full grid grid-cols-5 grid-rows-10 gap-3">
+          {/* Row 1 */}
+          <div className="relative w-full h-full bg-white rounded-tl-lg"></div>
+          <div className="relative w-full h-full bg-white col-span-3"></div>
+          <div className="relative w-full h-full bg-white rounded-tr-lg"></div>
+
+          {/* Row 2 */}
+          <div className="relative w-full h-full bg-white"></div>
+          <div className="relative w-full h-full col-span-3"></div>
+          <div className="relative w-full h-full bg-white row-span-3"></div>
+
+          {/* Row 3 */}
+          <div className="relative w-full h-full bg-white row-span-3"></div>
+          <div className="relative w-full h-full col-span-3"></div>
+          <div className="relative w-full h-full col-span-3"></div>
+          <div className="relative w-full h-full col-span-3"></div>
+          <div className="relative w-full h-full bg-white"></div>
+
+          {/* Row 5 */}
+          <div className="relative w-full h-full bg-white"></div>
+          <div className="relative w-full h-full col-span-3"></div>
+          <div className="relative w-full h-full bg-white"></div>
+
+          {/* Row 6 */}
+          <div className="relative w-full h-full bg-white row-span-3"></div>
+          <div className="relative w-full h-full col-span-3"></div>
+          <div className="relative w-full h-full bg-white row-span-3"></div>
+          <div className="relative w-full h-full col-span-3"></div>
+          <div className="relative w-full h-full col-span-3"></div>
+
+          {/* Row 10 */}
+          <div className="relative w-full h-full bg-white rounded-bl-lg"></div>
+          <div className="relative w-full h-full bg-white"></div>
+          <div className="relative w-full h-full bg-white col-span-2"></div>
+          <div className="relative w-full h-full bg-white rounded-br-lg"></div>
+        </div>
+      </div>
     </div>
   );
 };
