37927a362f44e41298847c9363a4768fabbeac09
admin <admin@adminui-noteubug.local>
2024-07-27 05:57:33 +0900

    feat: slot 자연스럽게

--- src/components/RollingArea.jsx
+++ src/components/RollingArea.jsx
@@ -7,27 +7,16 @@ const RollingArea = ({ content, isRolling, index }) => {
     next: 1,
   });
 
+  const [isStarted, setIsStarted] = useState(false);
   useEffect(() => {
     const contentLength = content.length;
     const intervalDelay = 200; // 200ms마다 롤링 업데이트
     const initialDelay = index * 500; // 각 인덱스에 대해 1초씩 딜레이
 
-    const interval = setInterval(() => {
-      if (isRolling) {
-        setCurrentIndex((indexes) => {
-          const newCurrent = (indexes.current + 1) % contentLength;
-          const newNext = (newCurrent + 1) % contentLength;
-          return {
-            prev: indexes.current,
-            current: newCurrent,
-            next: newNext,
-          };
-        });
-      }
-    }, intervalDelay);
-
+    // 초기 딜레이 후 롤링을 시작합니다.
     const timeout = setTimeout(() => {
       if (isRolling) {
+        setIsStarted(true); // 롤링을 시작하도록 상태를 업데이트합니다.
         setCurrentIndex((indexes) => {
           const newCurrent = (indexes.current + 1) % contentLength;
           const newNext = (newCurrent + 1) % contentLength;
@@ -40,11 +29,30 @@ const RollingArea = ({ content, isRolling, index }) => {
       }
     }, initialDelay);
 
+    // `isStarted`가 true로 변경될 때만 interval을 설정합니다.
+    const interval = isStarted
+      ? setInterval(() => {
+          console.log(index);
+          if (isRolling) {
+            setCurrentIndex((indexes) => {
+              const newCurrent = (indexes.current + 1) % contentLength;
+              const newNext = (newCurrent + 1) % contentLength;
+              return {
+                prev: indexes.current,
+                current: newCurrent,
+                next: newNext,
+              };
+            });
+          }
+        }, intervalDelay)
+      : null;
+
+    // 컴포넌트 언마운트 시 interval 및 timeout을 클리어합니다.
     return () => {
-      clearInterval(interval);
+      if (interval) clearInterval(interval);
       clearTimeout(timeout);
     };
-  }, [isRolling]);
+  }, [isRolling, isStarted]); // `isRolling`과 `isStarted` 의존성 추가
 
   return (
     <div className="flex flex-col w-full h-[200px] border border-black rounded-2xl bg-white overflow-hidden relative">
